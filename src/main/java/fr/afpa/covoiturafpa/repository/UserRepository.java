fe5e8b0ce67bdf8d7e6574c4421b5ca6cf6ade14
Morgan <tranquard.morgan@gmail.com>
2022-09-06 08:29:29 +0200

    fix repository

--- src/main/java/fr/afpa/covoiturafpa/repository/UserRepository.java
+++ src/main/java/fr/afpa/covoiturafpa/repository/UserRepository.java
@@ -1,9 +1,11 @@
 package fr.afpa.covoiturafpa.repository;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 import fr.afpa.covoiturafpa.model.User;
 
+@Repository
 public interface UserRepository extends CrudRepository<User, Integer> {
     
 }
