efd15c0050b8c9a22515e835314164eafada901a
Juju-D <72973194+Juju-D@users.noreply.github.com>
2022-09-09 11:36:36 +0200

    update dayWeek

---
+++ src/main/java/fr/afpa/covoiturafpa/model/DayWeek.java
@@ -0,0 +1,60 @@
+package fr.afpa.covoiturafpa.model;
+
+import java.util.Set;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.ManyToAny;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+
+@Entity
+@Table(name = "day_week")
+public class DayWeek {
+
+    @Id
+    @Column(name= "id_day_week")
+    private Integer idDayWeek;
+
+    
+    @Enumerated(EnumType.STRING)
+    @Column
+    private Day name;
+
+    @JsonBackReference
+    @ManyToMany(mappedBy = "daysWeek")
+    private Set<RecurringRide> rides;
+
+    public Set<RecurringRide> getRides() {
+        return rides;
+    }
+
+    public void setRides(Set<RecurringRide> rides) {
+        this.rides = rides;
+    }
+
+    public Integer getIdDayWeek() {
+        return idDayWeek;
+    }
+
+    public void setIdDayWeek(Integer idDayWeek) {
+        this.idDayWeek = idDayWeek;
+    }
+
+    public Day getName() {
+        return name;
+    }
+
+    public void setName(Day name) {
+        this.name = name;
+    }
+    public DayWeek() {
+
+    }
+}
--- src/main/java/fr/afpa/covoiturafpa/model/RecurringRide.java
+++ src/main/java/fr/afpa/covoiturafpa/model/RecurringRide.java
@@ -1,11 +1,17 @@
 package fr.afpa.covoiturafpa.model;
 
 import java.time.LocalDate;
+import java.util.Set;
 
 import javax.persistence.Column;
 import javax.persistence.DiscriminatorValue;
 import javax.persistence.Entity;
+import javax.persistence.JoinTable;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToMany;
 import javax.persistence.Table;
+
+import com.fasterxml.jackson.annotation.JsonManagedReference;
 
 
 @Entity
@@ -18,6 +24,11 @@ public class RecurringRide extends Ride {
 
     @Column
     private LocalDate ending;
+
+    @JoinTable( name = "recurring_days", joinColumns = @JoinColumn(name = "id_ride"), inverseJoinColumns = @JoinColumn(name = "id_day_week"))
+    @JsonManagedReference
+    @ManyToMany
+    private Set<DayWeek> daysWeek;
 
     public LocalDate getBeginning() {
         return beginning;
@@ -33,6 +44,14 @@ public class RecurringRide extends Ride {
 
     public void setEnding(LocalDate ending) {
         this.ending = ending;
+    }
+
+    public Set<DayWeek> getDaysWeek() {
+        return daysWeek;
+    }
+
+    public void setDaysWeek(Set<DayWeek> daysWeek) {
+        this.daysWeek = daysWeek;
     }
 
     public RecurringRide() {
