7e9f3bf50799d51b0f56dcdf7acc3afbc8a21fd0
Cyril Menant <menant.cyril@laposte.net>
2022-09-14 08:47:08 +0200

    changement fonction distance sql

--- scripts_sql/fonction_distance.sql
+++ scripts_sql/fonction_distance.sql
@@ -2,6 +2,5 @@ CREATE OR REPLACE FUNCTION covoiturafpa.get_distance(latitude_1 double precision
  RETURNS double precision
  LANGUAGE sql
  IMMUTABLE STRICT
-RETURN sqrt((((latitude_1 - latitude_2) * (latitude_1 - latitude_2)) + ((longitude_1 - longitude_2) * (longitude_1 - longitude_2))))
-;
+RETURN sqrt((((latitude_1 - latitude_2) * (latitude_1 - latitude_2)) + ((longitude_1 - longitude_2) * (longitude_1 - longitude_2))));
 GRANT ALL ON FUNCTION covoiturafpa.get_distance(float8, float8, float8, float8) TO "user";
