3936b7176a67b5e050568c82c6378e493778d0f6
qotsa7 <mtranqua@gmail.fr>
2022-09-15 13:59:05 +0200

    ajout bcryptpasswordencoder

--- src/main/java/fr/afpa/covoiturafpa/Application.java
+++ src/main/java/fr/afpa/covoiturafpa/Application.java
@@ -2,6 +2,8 @@ package fr.afpa.covoiturafpa;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
 public class Application {
@@ -9,4 +11,9 @@ public class Application {
         SpringApplication.run(Application.class, args);
 
     }
+
+    @Bean
+	PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
 }
