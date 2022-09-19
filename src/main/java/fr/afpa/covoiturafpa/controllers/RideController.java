b085306a1c5ac1b6607e4aca4dd73af44082f766
Cyril Menant <menant.cyril@laposte.net>
2022-09-19 17:00:30 +0200

    queries au feu

--- src/main/java/fr/afpa/covoiturafpa/controllers/RideController.java
+++ src/main/java/fr/afpa/covoiturafpa/controllers/RideController.java
@@ -1,6 +1,11 @@
 package fr.afpa.covoiturafpa.controllers;
 
 import org.springframework.http.MediaType;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -14,6 +19,9 @@ import org.springframework.web.bind.annotation.RestController;
 import fr.afpa.covoiturafpa.model.RecurringRide;
 import fr.afpa.covoiturafpa.model.Ride;
 import fr.afpa.covoiturafpa.repository.RideRepository;
+import fr.afpa.covoiturafpa.model.City;
+import fr.afpa.covoiturafpa.model.DayWeek;
+import fr.afpa.covoiturafpa.model.Destination;
 import fr.afpa.covoiturafpa.model.OneTimeRide;;
 
 @RestController
@@ -22,20 +30,32 @@ public class RideController {
     @Autowired
     private RideRepository rideRepository;
     
+
     // @CrossOrigin
     // @GetMapping(value = "/rides", produces = { MediaType.APPLICATION_JSON_VALUE })
     // @ResponseStatus(HttpStatus.OK)
     // public Iterable<Ride> searchRelevantRides(@RequestBody Ride ride) {
     //     if (ride instanceof RecurringRide) {
-    //         return rideRepository.findRecurringRides(ride.getDestination(), ((RecurringRide) ride).getBeginning(), ((RecurringRide) ride).getEnding(), ((RecurringRide) ride).getDaysWeek() );
+    //         return searchRelevantRidesForRecurring(ride);
     //     }
     //     else {
-    //         return rideRepository.findOneTimeRides(ride.getDestination(), ((OneTimeRide) ride).getDepartureDay(), );
+    //         return searchRelevantRidesForOneTime(ride);
     //     }
     // }
 
-    
+    // public Iterable<Ride> searchRelevantRidesForRecurring(Ride ride) {
+    //     return rideRepository.findRecurringRides(ride.getDestination());
+    // }
 
+    // public Iterable<Ride> searchRelevantRidesForOneTime(Ride ride) {
+    //     Set<DayWeek> day = new HashSet<DayWeek>();
+    //     day.add(new DayWeek(((OneTimeRide) ride).getDepartureDay().getDayOfWeek()));
+    //     List<Ride> results = rideRepository.findOneTimeRides(ride.getDestination(), ((OneTimeRide) ride).getDepartureDay());
+    //     results.addAll(rideRepository.findRecurringRides(ride.getDestination()));
+    //     return results;
+    // }
+
+   
     @CrossOrigin
     @PostMapping(value = "/rides", consumes = { MediaType.APPLICATION_JSON_VALUE })
     @ResponseStatus(HttpStatus.OK)
@@ -49,4 +69,13 @@ public class RideController {
     public Ride book(@PathVariable(required = true) int id, @RequestBody String jsonString) {
         return null;
     }
+
+
+    @CrossOrigin
+    @GetMapping(value = "/cities", consumes = { MediaType.APPLICATION_JSON_VALUE })
+    @ResponseStatus(HttpStatus.OK)
+    public List<Destination> getCities(@RequestBody Destination destination) {
+        return rideRepository.findTest(((double) 59), ((double) 81));
+    }
+
 }
--- src/main/java/fr/afpa/covoiturafpa/repository/RideRepository.java
+++ src/main/java/fr/afpa/covoiturafpa/repository/RideRepository.java
@@ -9,6 +9,7 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import fr.afpa.covoiturafpa.model.City;
 import fr.afpa.covoiturafpa.model.DayWeek;
 import fr.afpa.covoiturafpa.model.Destination;
 import fr.afpa.covoiturafpa.model.Ride;
@@ -16,16 +17,16 @@ import fr.afpa.covoiturafpa.model.Ride;
 @Repository
 public interface RideRepository extends CrudRepository<Ride, Integer> {
 
-    // @Query("SELECT ride FROM Ride ride JOIN OneTimeRide WHERE ((:destination.city IN (SELECT d.city FROM Destination d WHERE d.city IS NOT NULL)) OR (distance()))")
+    // @Query("SELECT ride FROM Ride ride JOIN RecurringRide WHERE ((:destination.city.name IN (SELECT d.city.name FROM Destination d WHERE d.city IS NOT NULL)) OR (get_distance(:destination.latitude, :destination.longitude, ride.destination.latitude, ride.destination.longitude)))")
+    // public List<Ride> findRecurringRides(@Param("destination") Destination destination);
+   
+
+    // @Query("SELECT ride FROM Ride ride JOIN OneTimeRide WHERE ((:#{#destination.city.name} IN (SELECT d.city.name FROM Destination d WHERE d.city IS NOT NULL)) OR (get_distance(:#{#destination.latitude}, :#{#destination.longitude}, ride.destination.latitude, ride.destination.longitude))) AND ride.departureDay = :date")
     // public List<Ride> findOneTimeRides(@Param("destination") Destination destination, @Param("date") LocalDate date);
 
-    // @Query("SELECT ride FROM RecurringRide")
-    // public List<Ride> findRecurringRides(@Param("destination") Destination destination, @Param("start") LocalDate start, @Param("end") LocalDate end, @Param("days") Set<DayWeek> days);
+    @Query("SELECT d FROM Destination d WHERE function('get_distance', d.latitude, d.longitude, :a, :b) < 5")
+    public List<Destination> findTest(@Param("a") double a, @Param("b") double b);
 
-
-    @Query("SELECT ride FROM RecurringRide ride WHERE ride.beginning = :date")
-    public List<Ride> findRecurringRidesByDate(@Param("date") LocalDate date);
-
-    @Query("SELECT rde FROM Person usr JOIN RidePassenger rp ON usr.id = rp.id.idPerson JOIN Ride rde ON rp.id.idRide = rde.id WHERE rp.id.idPerson = :id")
+    @Query("SELECT ride FROM Person user JOIN RidePassenger rp ON user.id = rp.id.idPerson JOIN Ride ride ON rp.id.idRide = ride.id WHERE rp.id.idPerson = :id")
     public Iterable<Ride> findRidesOfPerson(@Param("id") int id);
 }
