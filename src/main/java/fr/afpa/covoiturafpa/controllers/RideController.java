356532a4a3d07470b7c20169d610a8f1caf1d267
qotsa7 <mtranqua@gmail.fr>
2022-10-17 16:22:41 +0200

    modification endpoint ride/idride

--- src/main/java/fr/afpa/covoiturafpa/controllers/RideController.java
+++ src/main/java/fr/afpa/covoiturafpa/controllers/RideController.java
@@ -1,6 +1,7 @@
 package fr.afpa.covoiturafpa.controllers;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Optional;
 
@@ -9,13 +10,16 @@ import javax.transaction.Transactional;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.web.server.ErrorPage;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -51,7 +55,6 @@ public class RideController {
     @Autowired
     private NotificationRepository notificationRepository;
 
-
     @Autowired
     private CityRepository cityRepository;
 
@@ -66,27 +69,34 @@ public class RideController {
             ride = objectMapper.readValue(searchParams, Ride.class);
             if (ride instanceof RecurringRide) {
                 return searchRelevantRidesForRecurring(ride);
-            }
-            else {
+            } else {
                 return searchRelevantRidesForOneTime(ride);
             }
-        }
-        catch (JsonProcessingException e) {
+        } catch (JsonProcessingException e) {
             Logger logger = LoggerFactory.getLogger(CentreController.class);
             logger.error("Erreur dans la recherche de trajet : le JSON n'est pas exploitable.");
-        } 
+        }
         return null;
     }
 
     public Iterable<Ride> searchRelevantRidesForRecurring(Ride ride) {
-        return rideRepository.filterRecurringRidesByDays(rideRepository.findRecurringRides(ride.getDestination().getCity().getName(), ride.getDestination().getLatitude(), ride.getDestination().getLongitude(), ((RecurringRide) ride).getBeginning(), ((RecurringRide) ride).getEnding(), ride.getDestination().getIsFromAfpa()), ((RecurringRide) ride).getDaysWeek());
+        return rideRepository
+                .filterRecurringRidesByDays(rideRepository.findRecurringRides(ride.getDestination().getCity().getName(),
+                        ride.getDestination().getLatitude(), ride.getDestination().getLongitude(),
+                        ((RecurringRide) ride).getBeginning(), ((RecurringRide) ride).getEnding(),
+                        ride.getDestination().getIsFromAfpa()), ((RecurringRide) ride).getDaysWeek());
     }
 
     public Iterable<Ride> searchRelevantRidesForOneTime(Ride ride) {
-        List<Ride> results = rideRepository.findOneTimeRides(ride.getDestination().getCity().getName(), ride.getDestination().getLatitude(), ride.getDestination().getLongitude(), ((OneTimeRide) ride).getDepartureDay(), ride.getDestination().getIsFromAfpa());
+        List<Ride> results = rideRepository.findOneTimeRides(ride.getDestination().getCity().getName(),
+                ride.getDestination().getLatitude(), ride.getDestination().getLongitude(),
+                ((OneTimeRide) ride).getDepartureDay(), ride.getDestination().getIsFromAfpa());
         List<DayWeek> days = new ArrayList<DayWeek>();
         days.add(new DayWeek(((OneTimeRide) ride).getDepartureDay().getDayOfWeek()));
-        results.addAll(rideRepository.filterRecurringRidesByDays(rideRepository.findRecurringRides(ride.getDestination().getCity().getName(), ride.getDestination().getLatitude(), ride.getDestination().getLongitude(), ((OneTimeRide) ride).getDepartureDay(), ((OneTimeRide) ride).getDepartureDay(), ride.getDestination().getIsFromAfpa()), days));
+        results.addAll(rideRepository.filterRecurringRidesByDays(rideRepository.findRecurringRides(
+                ride.getDestination().getCity().getName(), ride.getDestination().getLatitude(),
+                ride.getDestination().getLongitude(), ((OneTimeRide) ride).getDepartureDay(),
+                ((OneTimeRide) ride).getDepartureDay(), ride.getDestination().getIsFromAfpa()), days));
         return results;
     }
 
@@ -100,14 +110,17 @@ public class RideController {
         try {
             if (ride instanceof RecurringRide) {
                 RecurringRide recurringRide = (RecurringRide) ride;
-                existingRide = rideRepository.findRecurringRidesByDateTimeAndDestination(recurringRide.getDestination().getCity().getName(), recurringRide.getBeginning(), recurringRide.getEnding(), recurringRide.getDestination().getIsFromAfpa());
-            }
-            else {
+                existingRide = rideRepository.findRecurringRidesByDateTimeAndDestination(
+                        recurringRide.getDestination().getCity().getName(), recurringRide.getBeginning(),
+                        recurringRide.getEnding(), recurringRide.getDestination().getIsFromAfpa());
+            } else {
                 OneTimeRide oneTimeRide = (OneTimeRide) ride;
-                existingRide = rideRepository.findOneTimeRidesByDateTimeAndDestination(oneTimeRide.getDestination().getCity().getName(), oneTimeRide.getDepartureDay(), oneTimeRide.getDepartureTime(), oneTimeRide.getDestination().getIsFromAfpa());
+                existingRide = rideRepository.findOneTimeRidesByDateTimeAndDestination(
+                        oneTimeRide.getDestination().getCity().getName(), oneTimeRide.getDepartureDay(),
+                        oneTimeRide.getDepartureTime(), oneTimeRide.getDestination().getIsFromAfpa());
             }
-            if(existingRide.size() == 0) {
-                //Début de la création d'un ride
+            if (existingRide.size() == 0) {
+                // Début de la création d'un ride
                 Optional<Person> driver = personRepository.findById(ride.getCar().getPerson().getId());
                 ride.setDriver(driver.get());
                 Optional<City> city = cityRepository.findByName(ride.getDestination().getCity().getName());
@@ -127,8 +140,7 @@ public class RideController {
                 }
                 return rideRepository.save(ride);
             }
-        }
-        catch (Exception e) {
+        } catch (Exception e) {
             Logger logger = LoggerFactory.getLogger(RideController.class);
             logger.error("Erreur : Impossible de créer un ride déjà existant");
         }
@@ -138,13 +150,20 @@ public class RideController {
     @CrossOrigin
     @PutMapping(value = "/rides/{idRide}")
     @ResponseStatus(HttpStatus.OK)
-    public void book(@PathVariable(required = true) int idRide, @RequestParam int idPassenger) {
+    public ResponseEntity<HashMap<String, String>> book(@PathVariable(required = true) int idRide, @RequestParam int idPassenger) throws Exception {
         Ride ride = rideRepository.findById(idRide).get();
         Person passenger = personRepository.findById(idPassenger).get();
-        if (ride.addBooking(passenger)) {
+        HashMap<String, String> responseMessage = new HashMap<String, String>();
+        if (!ride.hasBooking(passenger) && ride.addBooking(passenger)) {
             rideRepository.save(ride);
-            Notification newNotification = new Notification(Notification.TypeNotif.NEW_RESERVATION, NotifContentBuilder.createNewBookingContent(passenger, ride), ride.getDriver());
+            Notification newNotification = new Notification(Notification.TypeNotif.NEW_RESERVATION,
+                NotifContentBuilder.createNewBookingContent(passenger, ride), ride.getDriver());
             notificationRepository.save(newNotification);
+            responseMessage.put("message", "Demande de réservation effectuée");
+            return new ResponseEntity<HashMap<String, String>>(responseMessage, HttpStatus.OK);
+        } else {
+            responseMessage.put("message", "Réservation impossible, utilisateur déjà inscrit");
+            return new ResponseEntity<HashMap<String, String>>(responseMessage, HttpStatus.ACCEPTED); 
         }
     }
 
