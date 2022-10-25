28a79d1b1f4df03051d1a170e58d55ca586fdb1e
Ludo_E <ludovic.esperce@posteo.net>
2022-10-25 13:25:12 +0200

    Utilisation des ResponseEntity dans les endpoints de FuelController

--- src/main/java/fr/afpa/covoiturafpa/controllers/FuelController.java
+++ src/main/java/fr/afpa/covoiturafpa/controllers/FuelController.java
@@ -1,16 +1,18 @@
 package fr.afpa.covoiturafpa.controllers;
 
 import java.util.Optional;
+import java.util.stream.StreamSupport;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
-import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import fr.afpa.covoiturafpa.model.Fuel;
 import fr.afpa.covoiturafpa.repository.FuelRepository;
@@ -20,25 +22,48 @@ public class FuelController {
     
     @Autowired
     private FuelRepository fuelRepository;
-    
+
+    private Logger logger = LoggerFactory.getLogger(FuelController.class);
+
     @CrossOrigin
     @GetMapping(value = "/fuels", produces = { MediaType.APPLICATION_JSON_VALUE })
-    @ResponseStatus(HttpStatus.OK)
-    public Iterable<Fuel> list() {
-        return fuelRepository.findAll();
+    public ResponseEntity<Iterable<Fuel>> list() {
+        Iterable<Fuel> fuels = fuelRepository.findAll();
+
+        if (StreamSupport.stream(fuels.spliterator(), false).count() > 0) {
+            return ResponseEntity.ok(fuels);
+        } else {
+            return ResponseEntity.noContent().build();
+        }
     }
 
     @CrossOrigin
     @GetMapping(value = "/fuels/{id}", produces = { MediaType.APPLICATION_JSON_VALUE })
-    @ResponseStatus(HttpStatus.OK)
-    public Optional<Fuel> get(@PathVariable(required = true) int id) {
-        return fuelRepository.findById(id);
+    public ResponseEntity<Fuel> get(@PathVariable(required = true) int id) {
+        Optional<Fuel> foundFuel = null;
+        try {
+            foundFuel = fuelRepository.findById(id);
+        } catch (IllegalArgumentException exception) {
+            logger.error("Le paramètre passé au endpoint n'est pas correct.");
+            return ResponseEntity.badRequest().build();
+        }
+
+        if (foundFuel.isPresent()) {
+            return ResponseEntity.ok(foundFuel.get());
+        } else {
+            return ResponseEntity.notFound().build();
+        }
     }
     
     @CrossOrigin
     @PutMapping(value = "/fuels/{id}", consumes = { MediaType.APPLICATION_JSON_VALUE })
-    @ResponseStatus(HttpStatus.OK)
-    public Fuel updatePrice(@PathVariable(required = true) int id, @RequestBody Fuel fuel) {
-        return fuelRepository.save(fuel);
+    public ResponseEntity<Fuel> updatePrice(@PathVariable(required = true) int id, @RequestBody Fuel fuel) {
+        try {
+            fuelRepository.save(fuel);
+        } catch (IllegalArgumentException exception) {
+            logger.error("Le paramètre passé au endpoint n'est pas correct.");
+            return ResponseEntity.badRequest().build();
+        }
+        return ResponseEntity.ok().build();
     }
 }
