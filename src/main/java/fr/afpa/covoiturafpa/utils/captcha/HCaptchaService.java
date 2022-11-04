80ad3384481821732ecc9ffb2caeff65b9e8be0e
Cyril Menant <menant.cyril@laposte.net>
2022-11-04 11:19:36 +0100

    rajout d'un JsonNode d'échec en cas d'erreur

--- src/main/java/fr/afpa/covoiturafpa/utils/captcha/HCaptchaService.java
+++ src/main/java/fr/afpa/covoiturafpa/utils/captcha/HCaptchaService.java
@@ -13,6 +13,8 @@ import org.springframework.stereotype.Component;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.JsonNodeFactory;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 
 @Component
 public class HCaptchaService {
@@ -68,7 +70,9 @@ public class HCaptchaService {
             return mapper.readTree(response.body());
             } catch (Exception e) {
                 e.printStackTrace();
-                return null;
+                ObjectNode successJson = JsonNodeFactory.instance.objectNode();
+                successJson.put("success", false);
+                return successJson;
             }
     }
 }
